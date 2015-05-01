e836f3911116af2ead2d46ad0a1c9b91908823a0
TurtleHunter <turtlehunter@users.noreply.github.com>
2015-05-01 17:50:52 -0300

    Added gameType 1/2

--- fgame.h
+++ fgame.h
@@ -14,15 +14,18 @@ public:
     QString getPath();
     QString getArgs();
     QString getAbsPath();
+    QString getType();
     void setName(QString val);
     void setExe(QString val);
     void setPath(QString val);
     void setArgs(QString val);
+    void setType(QString val);
 private:
     QString gameName;
     QString gameExe;
     QString gamePath;
     QString gameArgs;
+    QString gameType;
 };
 
 #endif // FGAME_H
