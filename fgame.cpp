1ff1067efadc2d05ef76e24df58498875612ba13
TurtleHunter <turtlehunter@users.noreply.github.com>
2015-05-01 17:53:09 -0300

    added gameType 2/2
    Made using web browser so watch out

--- fgame.cpp
+++ fgame.cpp
@@ -6,6 +6,7 @@ FGame::FGame()
     this->gameExe = "Unknown";
     this->gamePath = "Unknown";
     this->gameArgs = "Unknown";
+    this->gameType = "Unknown";
 }
 
 QString FGame::getName() {
@@ -33,6 +34,11 @@ QString FGame::getAbsPath()
     return RawString.arg(this->getPath(), this->getExe(), this->getArgs());
 }
 
+QString FGame::getType()
+{
+    return this->gameType;
+}
+
 void FGame::setName(QString val)
 {
     this->gameName = val;
@@ -51,4 +57,9 @@ void FGame::setPath(QString val)
 void FGame::setArgs(QString val)
 {
     this->gameArgs = val;
+}
+
+void FGame::setType(QString val)
+{
+    this->gameType = val;
 }
