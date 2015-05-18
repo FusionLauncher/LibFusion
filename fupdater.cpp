9cd1932101a2756a6f22f7623b3b14769196a49c
lolsatbirds <lolsatbirds@gmail.com>
2015-05-17 22:11:15 -0400

    Added FUpdater. Fixed (or broke) FDB.
    FUpdater is a class that contains the functions that will be used for
    the auto-updater. The functions currently do nothing. Also fixed/broke
    FDB. Added a return behind the exec()'s at the end of FDB::addTextPref
    and FDB::updateTextPref to remove errors.

---
+++ fupdater.cpp
@@ -0,0 +1,38 @@
+#include "fupdater.h"
+
+FUpdater::FUpdater()
+{
+
+}
+
+//Get FusionLib version from api.
+void FUpdater::getLibraryVersion()
+{
+
+    //TODO
+}
+
+//Get FusionClient version from api.
+void FUpdater::getClientVersion()
+{
+
+    //TODO
+}
+
+//Compare downloaded library version with library version from api.
+bool FUpdater::isCurrentLibrary(QString thisLibrary)
+{
+
+    return "";
+    //TODO
+}
+
+//Compare downloaded client version with client version from api.
+bool FUpdater::isCurrentClient(QString thisClient)
+{
+
+    return "";
+    //TODO
+}
+
+//TODO: Add function to download things.
