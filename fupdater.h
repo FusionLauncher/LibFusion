f4ae0a36bd2086ecd3f7c8d2d473dc88ddf57f6d
lolsatbirds <lolsatbirds@gmail.com>
2015-05-17 22:05:41 -0400

    dads

---
+++ fupdater.h
@@ -0,0 +1,12 @@
+#ifndef FUPDATER_H
+#define FUPDATER_H
+
+#include <QObject>
+
+class FUpdater
+{
+public:
+    FUpdater();
+};
+
+#endif // FUPDATER_H
--- libfusion.h
+++ libfusion.h
@@ -8,6 +8,11 @@ class LIBFUSIONSHARED_EXPORT LibFusion
 
 public:
     LibFusion();
+    void getLibraryVersion();
+    void getClientVersion();
+    bool isCurrentLibrary(QString thisLibrary);
+    bool isCurrentClient(QString thisClient);
+    //TODO: Add function to download things.
 };
 
 #endif // LIBFUSION_H
