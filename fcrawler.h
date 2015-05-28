bae79a91cc76ddf787935a1b6f9a9eb1b46538dd
BurrrY <mail@florian-bury.de>
2015-05-28 20:03:18 +0200

    Add: Documentation

--- fcrawler.h
+++ fcrawler.h
@@ -12,11 +12,43 @@ class LIBFUSIONSHARED_EXPORT FCrawler
 public:
     FCrawler();
     ~FCrawler();
+
+    /**
+     * @brief scanAllFolders for new Games
+     *
+     * Start Scans in Folders based on their detected Type.
+     * Folders are defined in the DB, Table: watchedFolders
+     */
     void scanAllFolders();
 private:
     FDB db;
+
+    /**
+     * @brief getType
+     * @param folder Folder to Check
+     * @return Type of Folder
+     *
+     * Tries to get the Folder-Type, based on its content. Actually, only Steam is implemented
+     */
     FGameType getType(QDir folder);
+
+    /**
+     * @brief getSteamGames Scans a Steam-Library for Games
+     * @param folder Folder to Check
+     *
+     * Scans a SteamApps-Folder for Games. Adds all Missing games to the Lib.
+     */
     void getSteamGames(QDir folder);
+
+    /**
+     * @brief getOriginGames  Scans the Origin Logfile for new Games and adds them
+     *
+     * Reads the Logfile, located in "C:\\ProgramData\\Origin\\Logs\\Client_Log.txt"; for new Games
+     * Adds them, if they are not found in the Library.
+     *
+     * Succeeds for 80% of games.
+     *
+     */
     void getOriginGames();
 };
 
