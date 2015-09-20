f7790b39a00364a73ff18e96b4a64222ffaa47ad
Florian Bury <dev@florian-bury.de>
2015-09-20 20:43:50 +0200

    Fix a problem where the working dir differs for each application
    Now, the getWorkingDir returns always the same Directory.
    Previously it the Return dependet on the executed Program (i.e. Client, CLI or Updater)
    Now it returns the values which was returned with the Client.

--- libfusion.cpp
+++ libfusion.cpp
@@ -13,11 +13,20 @@ LibFusion::LibFusion()
 
 QDir LibFusion::getWorkingDir()
 {
+    QString p;
+
     #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
-    return QDir(QDesktopServices::storageLocation(QDesktopServices::DataLocation));
+        p = QDir(QDesktopServices::storageLocation(QDesktopServices::DataLocation));
     #else
-    return QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+        p = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
     #endif
+
+    int idx = p.lastIndexOf("/");
+    if(idx==-1)
+        idx = p.lastIndexOf("\\");
+    p = p.left(idx+1);
+    p += "FusionClient";
+    return QDir(p);
 }
 
 bool LibFusion::makeSureWorkingDirExists()
