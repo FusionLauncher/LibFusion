ef94006fe61a6f2fbf97c1a01e990d7f0c23602a
burrry <dev@florian-bury.de>
2016-05-30 17:47:01 +0200

    Adding Test for intPrefs

--- LibFusionTest/tst_fusiontestsuit.cpp
+++ LibFusionTest/tst_fusiontestsuit.cpp
@@ -76,11 +76,40 @@ void FusionTestSuit::gameCreate()
 void FusionTestSuit::DatabasePrefs()
 {
     QCOMPARE(db.getTextPref("nonExistent"), QString());
+    QCOMPARE(db.getTextPref(""), QString());
     QCOMPARE(db.getTextPref("nonExistent", "defaultValue"), QString("defaultValue"));
 
     QVERIFY(db.addTextPref("newPref", "newPrefs' value"));
     QCOMPARE(db.getTextPref("newPref"), QString("newPrefs' value"));
     QCOMPARE(db.getTextPref("newPref", "with defaultVal"), QString("newPrefs' value"));
+
+//delete Pref, getPref should now return an empty string
+    QVERIFY(db.deletePref("newPref"));
+    QCOMPARE(db.getTextPref("newPref"), QString());
+
+   //#################
+   //INT-PREFS
+    QCOMPARE(db.getIntPref("nonExistent"), -1);
+    QCOMPARE(db.getIntPref(""), -1);
+    //get Default-value
+    QCOMPARE(db.getIntPref("nonExistent", 14),14); 
+
+    // create Prefs
+    QVERIFY(db.addIntPref("newPref", 33));
+    QCOMPARE(db.getIntPref("newPref"), 33); 
+    QCOMPARE(db.getIntPref("newPref", 53), 33);
+
+//delete Pref, getPref should now return an empty string
+    QVERIFY(db.deletePref("newPref"));
+    QCOMPARE(db.getIntPref("newPref"), -1);
+
+    QVERIFY(db.addIntPref("newPref1", 0));
+    QCOMPARE(db.getIntPref("newPref1"), 0); 
+    QVERIFY(db.addIntPref("newPref2", -13));
+    QCOMPARE(db.getIntPref("newPref2"), -13); 
+    QVERIFY(db.addIntPref("newPref3", 214356570));
+    QCOMPARE(db.getIntPref("newPref3"), 214356570); 
+
 }
 
 void FusionTestSuit::addGame()
